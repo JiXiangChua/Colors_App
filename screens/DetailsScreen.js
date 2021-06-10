dc67710716fd1554413ea88d885af03c9e6950a2
JI XIANG <jixiangpro@JIs-MacBook-Pro.local>
2021-06-10 14:57:54 +0800

    Adjusted black text with light background and white text with dark background

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function DetailsScreen({ route }) {
   //use {route} to access the received parameters. console.log(route) to see what is passed over.
   //route.params to access the data values received.
   const { red, green, blue } = route.params; //javascript destructuring. destructure the values into const red, green and blue that we can use in this file
+  const avgColor = (red + green + blue) / 3;
+  const textColor = avgColor < 100 ? "white" : "black";
 
   return (
-    <View>
-      <Text>Red: {red}</Text>
-      <Text>Green: {green}</Text>
-      <Text>Blue: {blue}</Text>
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: `rgba(${red}, ${green}, ${blue}, 1)` },
+      ]}
+    >
+      <Text style={{ color: textColor }}>Red: {red}</Text>
+      <Text style={{ color: textColor }}>Green: {green}</Text>
+      <Text style={{ color: textColor }}>Blue: {blue}</Text>
     </View>
   ); //rmb to use {red} to access javascript variables in react native codes.
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
